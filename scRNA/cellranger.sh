d11ad995747b13a22d84939ba0ac01ed8bc938bb
liangzhenhou <44781595+liangzhenhou@users.noreply.github.com>
2025-05-29 14:27:38 +0800

    Update cellranger.sh

--- scRNA/cellranger.sh
+++ scRNA/cellranger.sh
@@ -10,10 +10,6 @@
 #SBATCH --array=1
 
 
-startTime=`date +%Y%m%d-%H:%M:%S`
-startTime_s=`date +%s`
-################################
-
 
 WORKDIR='/data/liangzhen/jinhua_jilab_project/result/scRNA/cellranger'
 FASTQDIR='/data/liangzhen/jinhua_jilab_project/data/scRNA/230715_A00838_0920_BHFHJFDSX7'
@@ -23,12 +19,4 @@ sample=$(cat /data/liangzhen/jinhua_jilab_project/data/scRNA/230715_A00838_0920_
 SRA=${sample}
 
 /data/zhaolian/software/cellranger-7.1.0/bin/cellranger count --id=${sample} --localcores=24 --transcriptome=/data/liangzhen/10X_resource/reference/mouse/refdata-gex-mm10-2020-A --fastqs=${FASTQDIR} --sample=${SRA}
-
-#################################
-endTime=`date +%Y%m%d-%H:%M:%S`
-endTime_s=`date +%s`
-
-sumTime=$[ $endTime_s - $startTime_s ]
-
-echo "$startTime ---> $endTime" "Total:$sumTime seconds"
 
