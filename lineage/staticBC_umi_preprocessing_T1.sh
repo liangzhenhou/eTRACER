0d58a0de6f764462727c3430bb2d615efa09cd4c
liangzhenhou <44781595+liangzhenhou@users.noreply.github.com>
2025-05-29 10:22:40 +0800

    Add files via upload

---
+++ lineage/staticBC_umi_preprocessing_T1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH -J staticBC_umi_preprocessing_T1
+#SBATCH -o /data/liangzhen/jinhua_jilab_project/script/AMPLICON/a3026/log/staticBC_umi_preprocessing_T1.out       
+#SBATCH -e /data/liangzhen/jinhua_jilab_project/script/AMPLICON/a3026/log/staticBC_umi_preprocessing_T1.error
+#SBATCH -p fat
+#SBATCH -N 1                      # apply for one node
+#SBATCH -n 20
+#SBATCH --mem=100G
+#SBATCH -t 00:00:00                # time limitation
+
+
+outdir='/data/liangzhen/jinhua_jilab_project/result/DNA_Amplicon/T1/a3026/cassiopeia_result'
+fastq_path='/data/liangzhen/jinhua_jilab_project/data/DNA_Amplicon/a3026/T1/staticBC_R1.fastq.gz,/data/liangzhen/jinhua_jilab_project/data/DNA_Amplicon/a3026/T1/staticBC_R2.fastq.gz'
+
+/home/liangzhen/anaconda3/envs/cassiopeia/bin/python3.8 /data/liangzhen/jinhua_jilab_project/script/AMPLICON/a3026/staticBC_umi_preprocessing.py ${outdir} ${fastq_path}
