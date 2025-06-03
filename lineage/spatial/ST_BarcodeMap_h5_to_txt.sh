75cb0714ee9ade8bb5dbefa552b0ec8430db3068
liangzhenhou <44781595+liangzhenhou@users.noreply.github.com>
2025-06-03 23:43:31 +0800

    Delete lineage directory

--- lineage/spatial/ST_BarcodeMap_h5_to_txt.sh
+++
@@ -1,5 +0,0 @@
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/liangzhen/anaconda3/envs/R432/lib:/home/liangzhen/anaconda3/envs/ST_BarcodeMap/lib
-
-/home/liangzhen/tools/ST_BarcodeMap/ST_BarcodeMap-0.0.1 --in /syn1/liangzhen/jinhua_jilab_project/data/spatial/sunyannan0626024/X101SC24045330-Z01-J002_StereoSeq_release_20240625095526/Rawdata/YJH_ST_2/B03203C612.barcodeToPos.h5 --out /syn1/liangzhen/jinhua_jilab_project/result/DNA_Amplicon/SPATIAL/T2/barcodeToPos.txt --action 3
-
