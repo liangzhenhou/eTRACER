71055d5e0b4d323b38a437b2a837f593556ffcd4
liangzhenhou <44781595+liangzhenhou@users.noreply.github.com>
2025-06-03 14:08:38 +0800

    Add files via upload

---
+++ lineage/scRNA/step01_run_cassiopeia_evoling_barcode.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH -J cassiopeia_T2
+#SBATCH -o /synology/liangzhen/jinhua_jilab_project/script/AMPLICON/a3026/log/cassiopeia_T2.out       
+#SBATCH -e /synology/liangzhen/jinhua_jilab_project/script/AMPLICON/a3026/log/cassiopeia_T2.error
+#SBATCH -p all
+#SBATCH -N 1                      
+#SBATCH -n 15
+#SBATCH --mem=20G
+#SBATCH -t 00:00:00                
+#SBATCH --array=1-15
+
+gene_info=$(cat /synology/liangzhen/jinhua_jilab_project/reference/EPC_scRNA-seq_amplicon_reference.txt | sed -n ${SLURM_ARRAY_TASK_ID}p)
+cut_sites=(0 80 91 93 87 63 89 89 84 86 94 48 87 89 85 89)
+
+innerBC=`echo ${gene_info} | awk '{print $6}' `
+gene_name=`echo ${gene_info} | awk '{print $1}' `
+timePoint=T2
+cut_site=`echo ${cut_sites[${SLURM_ARRAY_TASK_ID}]} `
+fastq_path=/synology/liangzhen/jinhua_jilab_project/result/DNA_Amplicon/${timePoint}/a3026/fastq
+outdir=/synology/liangzhen/jinhua_jilab_project/result/DNA_Amplicon/${timePoint}/a3026/cassiopeia_result
+mkdir -p ${outdir}
+
+/home/liangzhen/anaconda3/envs/cassiopeia2/bin/python3.8 cassiopeia_evoling_barcode_func.py ${innerBC} ${gene_name}  ${cut_site} ${outdir} ${fastq_path}
